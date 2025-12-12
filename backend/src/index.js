1160e36b0d56e097d912f6ff577376fa1c8e6eba
Harish1604 <harish16042005@gmail.com>
2025-12-12 21:21:14 +0530

    test ci

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,26 @@ app.use(express.json());
 const client = require("prom-client");
 client.collectDefaultMetrics();
 
+// at top
+const client = require('prom-client');
+const httpRequestDurationMicroseconds = new client.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['method', 'route', 'status_code'],
+  buckets: [50, 100, 200, 300, 500, 1000]
+});
+client.collectDefaultMetrics();
+
+// wrap around handlers
+app.use((req, res, next) => {
+  const end = httpRequestDurationMicroseconds.startTimer();
+  res.on('finish', () => {
+    end({ method: req.method, route: req.path, status_code: res.statusCode });
+  });
+  next();
+});
+
+
 app.get("/metrics", async (req, res) => {
   res.set("Content-Type", client.register.contentType);
   res.end(await client.register.metrics());
